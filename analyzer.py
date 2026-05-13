cf41758ce6f1448e8740bbd64d804f4886e2560f
Sanchit <sanchitk866@gmail.com>
2026-05-13 21:09:27 +0530

    Add codon frequency analyzer

--- analyzer.py
+++ analyzer.py
@@ -13,3 +13,19 @@ def reverse_complement(sequence):
     """Calculate reverse complement of a DNA sequence"""
     dna = Seq(sequence)
     return dna.reverse_complement()
+def codon_frequency(sequence):
+    """Calculate frequency of each codon in a DNA sequence"""
+    codon_counts = {}
+
+    for i in range(0, len(sequence), 3):
+        codon = sequence[i:i+3]
+
+        if len(codon) == 3:
+            if codon in codon_counts:
+                codon_counts[codon] += 1
+            else:
+                codon_counts[codon] = 1
+
+    return codon_counts
+
+    return codon_counts
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from analyzer import gc_content, reverse_complement
+from analyzer import (
+    gc_content,
+    reverse_complement,
+    codon_frequency
+)
 
-dna_sequence = "ATGCGC"
+dna_sequence = "ATGCGTATGAAA"
 
 gc_result = gc_content(dna_sequence)
 reverse_result = reverse_complement(dna_sequence)
+codon_result = codon_frequency(dna_sequence)
 
 print("GC Content: {0:.2f}%".format(gc_result))
 print("Reverse Complement: {0}".format(reverse_result))
+print("Codon Frequency: {0}".format(codon_result))
